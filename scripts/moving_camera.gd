97f44a29bf00cae774cdee47ecc6e068ed02bc15
Just <37525404+justofisker@users.noreply.github.com>
2023-01-27 16:09:08 -0800

    Zoom towards mouse cursor

--- scripts/moving_camera.gd
+++ scripts/moving_camera.gd
@@ -1,13 +1,8 @@
 extends Camera2D
 
-var zoom_level = 1.0
+var zoom_level := 1.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-
-func _input(event):
+func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
 		if Input.is_mouse_button_pressed(BUTTON_LEFT):
 			position -= event.relative * zoom / 2
@@ -16,18 +11,16 @@ func _input(event):
 			if event.button_index == BUTTON_LEFT:
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			if event.button_index == BUTTON_WHEEL_DOWN:
-				zoom_level -= 1
-				zoom_level = clamp(zoom_level, 1.0, 8.0)
-				zoom = Vector2(1.0 / zoom_level, 1.0 / zoom_level)
+				zoom_towards(zoom_level - 1)
 			if event.button_index == BUTTON_WHEEL_UP:
-				zoom_level += 1
-				zoom_level = clamp(zoom_level, 0.0, 8.0)
-				zoom = Vector2(1.0 / zoom_level, 1.0 / zoom_level)
+				zoom_towards(zoom_level + 1)
 		else:
 			if event.button_index == BUTTON_LEFT:
 				Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+func zoom_towards(level: float, position: Vector2 = get_viewport().get_mouse_position()) -> void:
+	var old_zoom_amout := 1.0 / zoom_level
+	zoom_level = clamp(level, 1.0, 8.0)
+	var zoom_amount := 1.0 / zoom_level
+	global_position += (position - get_viewport().size / 2.0) * (old_zoom_amout - zoom_amount)
+	zoom = Vector2(zoom_amount, zoom_amount)
